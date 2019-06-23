89f9faed0df71ea642031091b64b5070e21afc86
Sammy H <shann053@uottawa.ca>
2019-06-22 22:51:28 -0400

    removed unecessary js

--- assets/js/fineart.js
+++ assets/js/fineart.js
@@ -3,41 +3,10 @@ const getLatentUrl = host + "randomLatent";
 const getPredictionUrl = host + "predict";
 getRandomFace(setLeft);
 
-
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-
-  } else if (typeof XDomainRequest != "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url, true);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
-}
 function hexToBase64(str) {
 	return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
 }
-/*
-function getRandomFace() {
-	const url = host + "randomFace";
-	$.getJSON(url, function(data) {
-		$("#leftimage").attr("src", "data:image/png;base64," + data["image"]);
-	});	
-}
-*/
+
 function setLeft(data, latent) {
   setImage(data, "left", latent);
 }
